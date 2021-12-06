bd1b9bc86813a9742a1347ee93287e127cb30982
Laura <law7676@outlook.com>
2021-12-05 20:00:57 -0600

    modified app.js and styles.css

--- app.js
+++ app.js
@@ -2,28 +2,47 @@ console.log('JS is connected');
 
 const list = document.getElementById('week-two');
 const completed = document.getElementById('completed-items');
+const allLis = document.querySelectorAll('li');
+
+let todos = [];
+let completedItems = [];
+
+// savedTodos.push({ task: newTodo.innerText, isCompleted: false });
 
 list.addEventListener('click', function(e){
   if (e.target.tagName === 'BUTTON'){
-    //addListItem(e.target);
     e.target.parentElement.remove();
     completed.append(e.target.parentElement);
+    let completedLi = e.target.parentElement;
+    completedLi.classList.toggle('completed');
+    completedItems.push({"task": completedLi.innerText});
+    /*if (this.classList.toggle('completed') === true){
+      completedItems.push(completedLi.innerText);
+    }*/
     e.target.remove();
   }
 });
 
-let listItems = [];
-listItems.push({"1": "Complete todo list assignment", "2": "Build memory game"})
 
+
+
+//listItems.push({"1": "Complete todo list assignment", "2": "Build memory game"})
+/*
 const addListItem = function(listItem){
   listItems = JSON.parse(localStorage.getItem(list)) || [];
   listItems.push({"listItem": listItem});
   localStorage.setItem('list', JSON.stringify(list));
 }
+*/
 
-
-const myName = "Laura";
-localStorage.setItem('myName', myName);
+//const myName = "Laura";
+//localStorage.setItem('myName', myName);
+localStorage.setItem("completed-todos", JSON.stringify(completedItems));
+localStorage.removeItem("myName");
+//localStorage.setItem('completed-todos', JSON.stringify(completedItems));
 //localStorage.setItem('list', JSON.stringify(list));
 //localStorage.removeItem("todo-list");
 //localStorage.setItem('todo-list', JSON.stringify(listItems));
+//const local = localStorage.setItem("completedTodos", JSON.stringify(completedItems));
+//localStorage.removeItem("completed-todos");
+//localStorage.removeItem("completedTodos");

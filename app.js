6dce7e32fd865560461dbe2e668147ad61257708
Laura White <law7676@outlook.com>
2021-12-06 10:45:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const form = document.querySelector('form');
 const list = document.querySelector('ol');
 const input = document.querySelector('#form-content');
 // get all list items
-const todos = [];
+//const todos = [];
 
 // retrieve items from local storage
 const loadedTodos = JSON.parse(localStorage.getItem('tasks')) || [];
+for (let i = 0; i < loadedTodos.length; i++){
+    const newTodo = document.createElement('li');
+    newTodo.innerText = loadedTodos[i].task;
+    list.appendChild(newTodo);
+}
 
+/*
+if (loadedTodos.length > 0){
+    const newTodo = document.createElement('li');
+    newTodo.innerText = input.value;
+    list.appendChild(newTodo);
+}*/
 
 // submit
 // when submitted append to list -- last item
@@ -17,11 +28,12 @@ form.addEventListener('submit', function(e){
   const newTodo = document.createElement('li');
   newTodo.innerText = input.value;
   list.appendChild(newTodo);
-  todos.push({task: newTodo.innerText});
-  localStorage.setItem('tasks', JSON.stringify(todos));
+  loadedTodos.push({task: newTodo.innerText});
+  localStorage.setItem('tasks', JSON.stringify(loadedTodos));
 });
 
-//localStorage.removeItem("completed-todos");
+localStorage.removeItem(loadedTodos);
+localStorage.clear();
 //localStorage.removeItem("list");
 
 
